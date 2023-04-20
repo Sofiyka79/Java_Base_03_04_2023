89c397c61fd605cce3002c9a8b4cea65db4e2644
Bondarenko Sofia <Sofiykapn@gmail.com>
2023-04-20 20:44:43 +0200

    korrekt averageCommand

---
+++ src/main/java/ua/hillel/bondarenko/lessons/lesson6/classWork/Main.java
@@ -0,0 +1,106 @@
+package ua.hillel.bondarenko.lessons.lesson6.classWork;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        for (int i=0; i<5; i++){
+//            System.out.println("Hello");
+//        }
+//        for (int i=0; i<=20; i+=2){
+//            System.out.println(i);
+//
+//            }
+//        for (int i=0;i++)
+//             {
+//
+//        }
+//        int num1=1;
+//        int num2=1;
+//        int sum;
+//
+//        System.out.println(num1 + " "+num2+" ");
+//
+//        for (int i=0; i<11; i++){
+//            sum = num1+num2;
+//            System.out.println(sum + "");
+//
+//            num1=num2;
+//            num2 = sum;
+
+//        for (int i = 0, j = 10; i< 10 &&j>5; i++, j++){
+//            System.out.println("i="+i);
+//            System.out.println("j="+j);
+//            System.out.println();
+//        }
+//        }
+//        int i = -1;
+//        while (true){
+//            i++;
+//            if (i == 5){
+//                continue;
+//            }
+//            if (i>10){
+//                break;
+//            }
+//
+//                System.out.println(i);
+//
+//        }
+
+//        int i=0;
+//        do{
+//          System.out.println(i);
+//          i++;
+//        }while (i<4);
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please enter integer");
+//
+//
+//        while (true) {
+//            if (scanner.hasNextInt()) {
+//                int userValue = scanner.nextInt();
+//                if (userValue >= 10 && userValue <= 20) {
+//                    System.out.println("user value = " + userValue);
+//                    break;
+//                }else {
+//                    System.out.println(" Enter int from 10 to 20");
+//                }
+//                } else {
+//                    System.out.println("Wrong data! Try again");
+//                    scanner.nextLine();
+//                }
+//            }
+
+
+        int time = 10;
+
+    while (true){
+
+
+        System.out.println(time);
+
+
+
+        if (time ==0){
+
+            System.out.println("Boom!!!!!!!");
+            break;
+        }
+
+
+        time--;
+    }
+
+
+
+
+
+
+
+
+        }
+
+    }
+
