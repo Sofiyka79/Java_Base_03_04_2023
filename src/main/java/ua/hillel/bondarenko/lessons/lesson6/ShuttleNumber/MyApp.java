fa579829df1f47cbdea5b5e157fa120a8f3e0d5c
Bondarenko Sofia <Sofiykapn@gmail.com>
2023-04-21 13:28:52 +0200

    после сотни 4 и 9

--- src/main/java/ua/hillel/bondarenko/lessons/lesson6/ShuttleNumber/MyApp.java
+++ src/main/java/ua/hillel/bondarenko/lessons/lesson6/ShuttleNumber/MyApp.java
@@ -7,18 +7,17 @@ package ua.hillel.bondarenko.lessons.lesson6.ShuttleNumber;
 public class MyApp {
     public static void main(String[] args) {
         int sum=100;
+        int hundred;
 
         for (int i = 1; i <= sum; i++) {
-            if ((i == 4 || i==9 || i/10 ==4 || i/10 == 9 || (i%10==9) || i%10==4 || ((i-100)/10)==4 || ((i-100)/10)==9 )) {
+            if ((i == 4 || i==9 || i/10 ==4 || i/10 == 9 || (i%10==9) || i%10==4 || (i/100)==4 || ((i/100)==9 )||(i%100)==4 || ((i%100)==9))) {
                 sum = sum+1;
                 continue;
 
 
         }
             System.out.println(i);
-            int d = ((i-100)/10);
 
-            System.out.println(d);
 
         }
 
