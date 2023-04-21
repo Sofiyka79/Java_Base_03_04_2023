5e4107a539b117ba67b1bd8df72b0ab0c64feaf3
Bondarenko Sofia <Sofiykapn@gmail.com>
2023-04-21 12:20:42 +0200

    ShuttleNumber

---
+++ src/main/java/ua/hillel/bondarenko/lessons/lesson6/ShuttleNumber/MyApp.java
@@ -0,0 +1,25 @@
+package ua.hillel.bondarenko.lessons.lesson6.ShuttleNumber;
+//У Японії числа 4 та 9 вважаються нещасливими.
+// Вам потрібно пронумерувати 100 космічних шатлів для
+// перевезення людей на Марс так, щоб у номерах шатлів не
+// траплялося нещасливих чисел. Напишіть функцію, яка
+// виводить усі номери таких шатлів.
+public class MyApp {
+    public static void main(String[] args) {
+        int sum=100;
+
+        for (int i = 1; i <= sum; i++) {
+            if ((i == 4 || i==9 || i/10 ==4 || i/10 == 9 || (i%10==9) || i%10==4 )) {
+                sum = sum+1;
+                continue;
+
+
+        }
+            System.out.println(i);
+
+
+        }
+
+
+    }
+}
