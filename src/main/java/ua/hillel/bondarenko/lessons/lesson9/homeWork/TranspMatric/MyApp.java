b45a77d6c77abe42b0ae6310beaf6a53b83abba3
Bondarenko Sofia <Sofiykapn@gmail.com>
2023-05-06 23:47:37 +0200

    Транспортировка матриц

--- src/main/java/ua/hillel/bondarenko/lessons/lesson9/homeWork/TranspMatric/MyApp.java
+++ src/main/java/ua/hillel/bondarenko/lessons/lesson9/homeWork/TranspMatric/MyApp.java
@@ -23,23 +23,24 @@ public class MyApp {
 
 
         int [][] matrix1 = new int [n][m];
+        int [][] matrix2 = new int [m][n];
 
         for (int i=0; i< matrix1.length; i++){
             for (int j=0;j < matrix1[i].length; j++){
                 matrix1[i][j]= (int) (Math.random()*101);
-                System.out.print(matrix1[i][j]+ "\t");
-
             }
             System.out.println();
         }
 
-  //      System.out.println();
+            for (int i=0; i< matrix1.length; i++){
+                for (int j=0;j < matrix1[i].length; j++){
+                    System.out.print(matrix1[i][j]+ "\t");
+                }
+                System.out.println();
+            }
 
-
-        int [][] matrix2 = new int [m][n];
-
-        for (int i=0; i< matrix2.length; i++){
-            for (int j=0;j < matrix2[i].length; j++){
+        for (int i=0; i< matrix1.length; i++){
+            for (int j=0;j < matrix1[i].length; j++){
                int temp = matrix1[i][j];
                matrix2[i][j]=matrix1[j][i];
                matrix2[j][i]=temp;
