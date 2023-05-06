47706b575b2554c5caf04d513bfb9cbb5dc01586
Bondarenko Sofia <Sofiykapn@gmail.com>
2023-05-06 20:20:13 +0200

    Транспортировка матриц

--- src/main/java/ua/hillel/bondarenko/lessons/lesson9/homeWork/TranspMatric/MyApp.java
+++ src/main/java/ua/hillel/bondarenko/lessons/lesson9/homeWork/TranspMatric/MyApp.java
@@ -1,18 +1,46 @@
 package ua.hillel.bondarenko.lessons.lesson9.homeWork.TranspMatric;
+
+import java.util.Scanner;
+
 //Транспонуйте матрицю M x N. Розміри M та N задаються з консолі.
 // Після введення повинні автоматично створюватися
 // два масиви розміром M x N перший та N x M другий.
+
+
 public class MyApp {
+
     public static void main(String[] args) {
-        int n=7;
-        int m=5;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Введите N ");
+        Integer n = sc.nextInt();
+        System.out.println("Введите M ");
+        Integer m = sc.nextInt();
+
+
 
         int [][] matrix1 = new int [n][m];
+
         for (int i=0; i< matrix1.length; i++){
-            for (int j=0;j < matrix1.length; j++){
-                System.out.println(matrix1[i][j]+"\t");
+            for (int j=0;j < matrix1[i].length; j++){
+                System.out.print(matrix1[i][j]+ "\t");
 
             }
+            System.out.println();
+        }
+
+        System.out.println();
+
+
+        int [][] matrix2 = new int [m][n];
+
+        for (int i=0; i< matrix2.length; i++){
+            for (int j=0;j < matrix2[i].length; j++){
+                System.out.print(matrix2[i][j]+ "\t");
+
+            }
+            System.out.println();
         }
     }
 }
