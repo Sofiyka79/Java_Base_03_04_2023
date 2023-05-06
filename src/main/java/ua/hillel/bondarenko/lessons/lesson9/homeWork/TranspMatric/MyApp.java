5c88c74029779ca5f26ba65f550b6002dd157bc9
Bondarenko Sofia <Sofiykapn@gmail.com>
2023-05-06 23:19:33 +0200

    Транспортировка матриц

--- src/main/java/ua/hillel/bondarenko/lessons/lesson9/homeWork/TranspMatric/MyApp.java
+++ src/main/java/ua/hillel/bondarenko/lessons/lesson9/homeWork/TranspMatric/MyApp.java
@@ -38,8 +38,8 @@ public class MyApp {
 
         int [][] matrix2 = new int [m][n];
 
-        for (int i=0; i< matrix1.length; i++){
-            for (int j=0;j < matrix1[i].length; j++){
+        for (int i=0; i< matrix2.length; i++){
+            for (int j=0;j < matrix2[i].length; j++){
                int temp = matrix1[i][j];
                matrix2[i][j]=matrix1[j][i];
                matrix2[j][i]=temp;
