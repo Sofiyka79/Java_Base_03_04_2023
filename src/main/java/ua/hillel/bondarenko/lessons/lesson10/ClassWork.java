42c3e38580887af818fb2721e2620633807d6d22
Bondarenko Sofia <Sofiykapn@gmail.com>
2023-05-11 12:29:40 +0200

    Методы дз персон инфо

---
+++ src/main/java/ua/hillel/bondarenko/lessons/lesson10/ClassWork.java
@@ -0,0 +1,12 @@
+package ua.hillel.bondarenko.lessons.lesson10;
+
+public class ClassWork {
+
+    public static void main(String[] args) {
+        printHello();
+    }
+
+    static void printHello(){
+        System.out.println("Hello");
+    }
+}
