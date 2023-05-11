06da0098d31c7e1b90c068e5296f384f264d3c0a
Bondarenko Sofia <Sofiykapn@gmail.com>
2023-05-11 18:35:52 +0200

    Методы дз персон инфо

--- src/main/java/ua/hillel/bondarenko/lessons/lesson10/PersonInfo/Person.java
+++ src/main/java/ua/hillel/bondarenko/lessons/lesson10/PersonInfo/Person.java
@@ -1,4 +1,7 @@
 package ua.hillel.bondarenko.lessons.lesson10.PersonInfo;
+
+import java.sql.SQLOutput;
+
 // Зробити клас Person та в ньому метод personInfo()
 //На вхід передавати йому чотири параметри: ім'я, прізвище, місто, телефон
 //Всередині методу формувати рядок у форматі:
@@ -10,7 +13,10 @@ package ua.hillel.bondarenko.lessons.lesson10.PersonInfo;
 //Зателефонувати громадянинові Jackie Chan з міста Shanghai можна за номером 12312412412.
 //Зателефонувати до громадян Sherlock Holmes з міста London можна за номером 37742123513.
 public class Person {
-     static void PersonInfo(String name, String surname, String City, Integer Number) {
+     static String PersonInfo(String name, String surname, String city, Integer Number) {
 
+         System.out.println("Зателефонувати громадянину" + +"ІМ'Я ПРІЗВИЩЕ"+name + "із міста" + "МІСТО можна за номером" + city + "ТЕЛЕФОН"+ Number);
+         String info;
+         return info;
     }
 }
