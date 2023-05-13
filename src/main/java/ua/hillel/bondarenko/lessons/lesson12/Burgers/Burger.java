c6535d083ee59c884cda3a2b513bd91722f09d86
Bondarenko Sofia <Sofiykapn@gmail.com>
2023-05-13 15:03:19 +0200

    Бургер

---
+++ src/main/java/ua/hillel/bondarenko/lessons/lesson12/Burgers/Burger.java
@@ -0,0 +1,43 @@
+package ua.hillel.bondarenko.lessons.lesson12.Burgers;
+
+public class Burger {
+    boolean meat;
+    boolean bun;
+    boolean mayo;
+    boolean duoMeat;
+    boolean cheese;
+    boolean salad;
+
+    public Burger(boolean meat, boolean bun, boolean cheese, boolean salad) {
+        this.meat = meat;
+        this.bun = bun;
+        this.cheese = cheese;
+        this.salad = salad;
+
+        System.out.println("Дієтичний бургер (без майонезу) булочка, м'ясо, сир, зелень" );
+    }
+
+    public Burger(boolean meat, boolean bun, boolean mayo, boolean cheese, boolean salad) {
+        this.meat = meat;
+        this.bun = bun;
+        this.mayo = mayo;
+        this.cheese = cheese;
+        this.salad = salad;
+
+        System.out.println("звичайний бургер (усі компоненти) булочка, м'ясо, сир, зелень, майонез");
+    }
+
+    public Burger(boolean meat, boolean bun, boolean mayo, boolean duoMeat, boolean cheese, boolean salad) {
+        this.meat = meat;
+        this.bun = bun;
+        this.mayo = mayo;
+        this.duoMeat = duoMeat;
+        this.cheese = cheese;
+        this.salad = salad;
+        System.out.println("З подвійним м'ясом булочка, м'ясо, сир, зелень, майонез");
+    }
+
+
+
+}
+
