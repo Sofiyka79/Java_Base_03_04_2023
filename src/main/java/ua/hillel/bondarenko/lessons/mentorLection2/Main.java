7dc1275cba81f53869a139bbc8ef0a0cdb6dd271
Bondarenko Sofia <Sofiykapn@gmail.com>
2023-05-29 19:56:10 +0200

    MentorLection2

---
+++ src/main/java/ua/hillel/bondarenko/lessons/mentorLection2/Main.java
@@ -0,0 +1,14 @@
+package ua.hillel.bondarenko.lessons.mentorLection2;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder
+                .append(" ")
+                .append("Bondarenko");
+
+        System.out.println(stringBuilder);
+    }
+}
