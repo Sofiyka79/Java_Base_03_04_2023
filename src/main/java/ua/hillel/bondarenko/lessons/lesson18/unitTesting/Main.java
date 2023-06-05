06bc05acf242417e781221e1bfef012b07fffeb5
Bondarenko Sofia <Sofiykapn@gmail.com>
2023-06-05 14:57:38 +0200

    UnitTest

---
+++ src/main/java/ua/hillel/bondarenko/lessons/lesson18/unitTesting/Main.java
@@ -0,0 +1,31 @@
+package ua.hillel.bondarenko.lessons.lesson18.unitTesting;
+
+import java.util.Arrays;
+
+import static ua.hillel.bondarenko.lessons.lesson18.unitTesting.Methods.*;
+
+//Додайте метод, який буде рахувати середнє арифметичне елементів масиву.
+//Зробіть метод, який прийматиме на вхід двомірну матрицю
+// (двовимірний масив) у та перевіряти чи є вона квадратною
+//В тестовому класі створіть тестову матрицю та передайте її в метод
+// тестованого класу
+public class Main {
+    public static void main(String[] args) {
+
+            int[] arr = {1, 2, 3, 4, 5};
+            double avg = average(arr);
+            System.out.println("Среднее арифметическое: " + avg);
+
+            int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+            boolean isSquare = matrixSquare(matrix);
+            System.out.println("Квадратная матрица: " + isSquare);
+
+
+
+
+
+
+    }
+
+
+}
---
+++ src/main/java/ua/hillel/bondarenko/lessons/lesson18/unitTesting/Methods.java
@@ -0,0 +1,33 @@
+package ua.hillel.bondarenko.lessons.lesson18.unitTesting;
+
+import java.util.Arrays;
+
+public class Methods {
+
+    public static double average(int[] arr) {
+        double sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum / arr.length;
+    }
+
+    public static boolean matrixSquare(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        if (rows != cols) {
+            return false;
+        }
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i].length != cols) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
+
+
+}
